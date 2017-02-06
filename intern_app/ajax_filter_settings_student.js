8ebb63b1d71dc3fee7f2cd3db9859767060d269f
Christian <christiansuasi@gmail.com>
2017-02-06 12:13:13 -0500

    fixed date

--- intern_app/ajax_filter_settings_student.js
+++
@@ -1,5 +0,0 @@
-var processing_url = 'index.php?ss=jobs&mode=list&s=jobs';
-var content_div = "#student_job_list_content";
-var type = "Student Job Search";
-var csp_subsystem = 'students';
-var owner_class = 'student';
--- intern_app/si_ei.js
+++
@@ -1,8 +0,0 @@
-// User Interface JS file
-// Store common JS functions here and reduce bandwidth!
-
-// break out of frames, allow loginas.
-//alert(self.name);
-
-//if (self.name.indexOf('loginas') == -1 && self.name.indexOf('hiddenframe') == -1 && self.name != 'popwinframe' && top.location != self.location) top.location.href = self.location.href;
-
